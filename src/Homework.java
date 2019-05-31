1b3dd6a93305f3798a31cb2e8f66f4b578d8dd70
Maryana <renvema@bigmir.net>
2019-05-31 08:03:43 +0300

    develop commit2

--- src/Homework.java
+++ src/Homework.java
@@ -1,29 +1,21 @@
 public class Homework {
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        if (((a&&b)&&(!c&&!d))||((a&&c)&&(!b&&!d))||((a&&d)&&(!b&&!c))||((b&&c)&&(!a&&!d))||((b&&d)&&(!a&&!c))||((d&&c)&&(!b&&!a)))
-        {
+        if ((a ^ b) && (c ^ d) || (a ^ c) && (b ^ d)) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
     public static int leapYearCount(int year) {
-        return year= year/4 - year/100 +year/400;
+        return year = year / 4 - year / 100 + year / 400;
     }
 
-    public static boolean doubleExpression(double a, double b, double c) {
+    final static double fault = 0.0001;
 
-        if (Math.abs((a + b) - c) <= 0.0001) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    public static boolean doubleExpression(double a, double b, double c) {
+        return (Math.abs((a + b) - c) <= fault) ? true : false;
     }
 
     public static int flipBit(int value, int bitIndex) {
-        return value ^ (1 << bitIndex-1); //
+        return value ^ (1 << bitIndex - 1); //
     }
-
 }
-
