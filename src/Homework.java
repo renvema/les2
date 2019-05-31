321ab3b8349e83f246341c76bfbdb4d477e92ec2
Maryana <renvema@bigmir.net>
2019-05-31 08:05:58 +0300

    develop commit2

--- src/Homework.java
+++ src/Homework.java
@@ -16,6 +16,6 @@ public class Homework {
     }
 
     public static int flipBit(int value, int bitIndex) {
-        return value ^ (1 << bitIndex - 1); //
+        return value ^ (1 << bitIndex - 1);
     }
 }
