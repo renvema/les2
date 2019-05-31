1df0bfe828e5691c92e384ae31ab37d697ae8506
Maryana <renvema@bigmir.net>
2019-05-31 12:03:30 +0300

    les2 commit2

--- src/Homework2.java
+++ src/Homework2.java
@@ -1,3 +1,4 @@
+//from stepik
 public class Homework2 {
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         if ((a ^ b) && (c ^ d) || (a ^ c) && (b ^ d)) {
